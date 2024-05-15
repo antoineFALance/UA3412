55b99cf7e9c1bc2cf5e78ca91702756b0b393acd
antoine <antoine.lance2@gmail.com>
2024-05-15 15:09:48 +0200

    sensor and gas_pulse 20240525

---
+++ tempprobe_sensor.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+
+path_to_sensor_directory="/home/antoine/Documents/MASTER/UA3412/temprobe_avg_hr"
+directory = os.fsencode(path_to_sensor_directory)
+df=pd.DataFrame(columns=['year','month','day','hour','value'])
+gasApplianceList=[]
+
+conn_string = 'postgresql://postgres:admin@localhost/IDEAL'
+db = create_engine(conn_string)
+conn = db.connect()
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    fullFileName=path_to_sensor_directory+'/'+filename
+    split= filename.split("_")
+    home=split[0]
+    room=split[1]
+    sensor_num=split[2]
+    sensor_type=split[3]
+    appliance=split[4]
+    df=pd.read_csv(fullFileName,sep=";")
+    df['home_id']=home.replace("home","")
+    df['room_id']=room.replace("room","")
+    df['sensor_id']=sensor_num.replace("sensor","")
+    df['sensor_type']=sensor_type
+    df['appliance'] = appliance.replace(".csv","")
+    df['unit']='celcius'
+    df=df[['sensor_id','home_id','room_id','sensor_type','appliance','year','month','day','time_extract','unit','value_hr']]
+    df.columns=['sensor_id','home_id','room_id','sensor_type','appliance','year','month','day','hour','unit','value']
+    df['value']=df['value']/10
+    df.to_sql('sensor',con=conn, if_exists='append', index=False)
+    print(file)
+
+conn.close()
