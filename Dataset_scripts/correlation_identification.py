c20960bc74b9b928cef87c652505ffbbd05043b2
antoine <antoine.lance2@gmail.com>
2024-06-15 15:20:05 +0200

    Modification du script correlation_identification.py 20240615

--- Dataset_scripts/correlation_identification.py
+++ Dataset_scripts/correlation_identification.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import os
+import re
 
 # Define a function to calculate mean
 def mean(arr):
@@ -21,11 +22,12 @@ def cross_correlation(x, y):
     correlation = numerator / denominator
     return correlation
 
-PATH_TO_INPUT_DIR_DATA="######"
-PATH_TO_OUTPUT_DIR_DATA="######"
+PATH_TO_INPUT_DIR_DATA=os.path.dirname(os.getcwd())+"/data_/main_dataset/"
+PATH_TO_OUTPUT_DIR_DATA=os.path.dirname(os.getcwd())+"/data_/signal_dataset/"
 directory = os.fsencode(PATH_TO_INPUT_DIR_DATA)
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
+    home_id=re.search('weather_(.*).csv', filename).group(1)
     fullFileName=PATH_TO_INPUT_DIR_DATA+filename
     # PATH_TO_INPUT_DATA= "/data_/weather_home62.csv"
     input_=pd.read_csv(fullFileName,sep=";")
@@ -34,7 +36,9 @@ for file in os.listdir(directory):
     input_['t']=range(len(input_['datetime']))
 
     # Essayer de créer un nuage de points depuis les équations pluitôt qu'un extrait
-    ref_signal=input_[(input_['datetime']>='2017-02-05 7:00:00') &(input_['datetime']<='2017-02-05 17:00:00')]['temp_value'].to_numpy()
+
+    #ref_signal=input_[(input_['datetime']>='2017-02-05 7:00:00') &(input_['datetime']<='2017-02-05 17:00:00')]['temp_value'].to_numpy()
+    ref_signal=np.array([17,18,19,20,21,21,20,19,18,17])
 
     correlation_list,idxList=[],[]
     for idx in range(input_['t'].shape[0]):
@@ -42,7 +46,7 @@ for file in os.listdir(directory):
         correlation_list.append(cross_correlation(window_signal,ref_signal))
         idxList.append([idx,idx+11])
 
-    bestIndex=[idxList[index] for index in range(len(idxList)) if correlation_list[index]>0.95]
+    bestIndex=[idxList[index] for index in range(len(idxList)) if correlation_list[index]>0.85]
     output=[input_['temp_value'].to_numpy()[window[0]:window[1]] for window in bestIndex]
     inputs=[input_[['t','temp','gas_value']].to_numpy()[window[0]:window[1],:] for window in bestIndex]
     data=[input_[['t','temp','gas_value','temp_value']].to_numpy()[window[0]:window[1],:] for window in bestIndex]
@@ -50,13 +54,11 @@ for file in os.listdir(directory):
 
     enum=0
     for inp in data:
-        dfTemp=pd.DataFrame(data=inp)
+        dfTemp=pd.DataFrame(data=inp,columns=['time','Text','gas','Tint'])
         dfTemp['num']=enum
-        test = [dfTemp[col].isnull().all() for col in dfTemp.columns]
-        if all(test!=np.nan):
-            dfInput=pd.concat([dfInput,dfTemp],axis=0)
-            enum+=1
+        dfInput=pd.concat([dfInput,dfTemp],axis=0)
+        enum+=1
 
-    dfInput.to_csv(PATH_TO_OUTPUT_DIR_DATA+"#####.csv")
+    dfInput.to_csv(PATH_TO_OUTPUT_DIR_DATA+home_id+"heat_signal.csv")
 
 print('ok')
