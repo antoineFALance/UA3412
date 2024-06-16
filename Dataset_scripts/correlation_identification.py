0a9203a10704e96ce6db05dc1b908885f81ff506
antoine <antoine.lance2@gmail.com>
2024-06-16 16:34:40 +0200

    Modification du script correlation_identification.py AJout des données heat_signal 20240616

--- Dataset_scripts/correlation_identification.py
+++ Dataset_scripts/correlation_identification.py
@@ -26,8 +26,10 @@ PATH_TO_INPUT_DIR_DATA=os.path.dirname(os.getcwd())+"/data_/main_dataset/"
 PATH_TO_OUTPUT_DIR_DATA=os.path.dirname(os.getcwd())+"/data_/signal_dataset/"
 directory = os.fsencode(PATH_TO_INPUT_DIR_DATA)
 for file in os.listdir(directory):
+    print(file)
     filename = os.fsdecode(file)
     home_id=re.search('weather_(.*).csv', filename).group(1)
+    print(home_id)
     fullFileName=PATH_TO_INPUT_DIR_DATA+filename
     # PATH_TO_INPUT_DATA= "/data_/weather_home62.csv"
     input_=pd.read_csv(fullFileName,sep=";")
@@ -49,16 +51,19 @@ for file in os.listdir(directory):
     bestIndex=[idxList[index] for index in range(len(idxList)) if correlation_list[index]>0.85]
     output=[input_['temp_value'].to_numpy()[window[0]:window[1]] for window in bestIndex]
     inputs=[input_[['t','temp','gas_value']].to_numpy()[window[0]:window[1],:] for window in bestIndex]
-    data=[input_[['t','temp','gas_value','temp_value']].to_numpy()[window[0]:window[1],:] for window in bestIndex]
+    data=[input_[['t','year','month','day','hour','temp','gas_value','temp_value']].to_numpy()[window[0]:window[1],:] for window in bestIndex]
     dfInput=pd.DataFrame()
 
     enum=0
     for inp in data:
-        dfTemp=pd.DataFrame(data=inp,columns=['time','Text','gas','Tint'])
-        dfTemp['num']=enum
-        dfInput=pd.concat([dfInput,dfTemp],axis=0)
-        enum+=1
+        dfTemp=pd.DataFrame(data=inp,columns=['time','year','month','day','hour','Text','gas','Tint'])
+        dfTemp['num'] = enum
+        if not dfTemp['gas'].isnull().all():
+            dfTemp.interpolate(method='linear',limit_area='inside',inplace=True)
+            dfTemp['gas'].fillna(dfTemp['gas'].min(),inplace=True)
+            dfInput=pd.concat([dfInput,dfTemp],axis=0)
+            enum+=1
 
-    dfInput.to_csv(PATH_TO_OUTPUT_DIR_DATA+home_id+"heat_signal.csv")
+    dfInput.to_csv(PATH_TO_OUTPUT_DIR_DATA+home_id+"heat_signal.csv",sep=";",index=False)
 
 print('ok')
