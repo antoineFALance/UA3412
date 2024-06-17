6563ce03c90b327c481ff0c393c0ca790a1fd31d
Antoine <antoine.lance2@gmail.com>
2024-06-17 11:08:25 +0200

    20240617

--- Time serie/time_serie_analysis.py
+++ Time serie/time_serie_analysis.py
@@ -1,3 +1,12 @@
 import pandas as pd
+import os
+import sys
+is_windows = hasattr(sys, 'getwindowsversion')
 
 # Données météo
+if is_windows:
+    PATH_TO_WEATHER_FORECAST=os.path.dirname(os.path.dirname(os.getcwd()))+"\\data_\\weather_forecast\\"
+else:
+    PATH_TO_WEATHER_FORECAST = os.path.dirname(os.path.dirname(os.getcwd())) + "/data_/weather_forecast/"
+
+df_weather = pd.read_csv(PATH_TO_WEATHER_FORECAST+"#####.csv")
